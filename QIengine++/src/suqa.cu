5b389b0c9bfe95bd2e9e4bbe03141d90e7fb099f
GreenKing <giuseppe.clemente93@gmail.com>
2020-03-05 10:54:29 +0100

    Fixed bug in pauli tensor product rotations

--- QIengine++/src/suqa.cu
+++ QIengine++/src/suqa.cu
@@ -549,10 +549,10 @@ void kernel_suqa_pauli_TP_rotation_x(double *const state_re, double *const state
             tmp_im1 = state_im[i_1];
             
             state_re[i_0] = tmp_re0*ctheta - tmp_im1*stheta;
-            state_im[i_0] = tmp_im0*stheta + tmp_re1*stheta;
+            state_im[i_0] = tmp_im0*ctheta + tmp_re1*stheta;
 
             state_re[i_1] = tmp_re1*ctheta - tmp_im0*stheta; 
-            state_im[i_1] = tmp_im1*stheta + tmp_re0*stheta;
+            state_im[i_1] = tmp_im1*ctheta + tmp_re0*stheta;
         }
         i_0+=gridDim.x*blockDim.x;
     }
@@ -574,10 +574,10 @@ void kernel_suqa_pauli_TP_rotation_y(double *const state_re, double *const state
             tmp_im1 = state_im[i_1];
             
             state_re[i_0] = tmp_re0*ctheta + tmp_re1*stheta;
-            state_im[i_0] = tmp_im0*stheta + tmp_im1*stheta;
+            state_im[i_0] = tmp_im0*ctheta + tmp_im1*stheta;
 
             state_re[i_1] = tmp_re1*ctheta - tmp_re0*stheta; 
-            state_im[i_1] = tmp_im1*stheta - tmp_im0*stheta;
+            state_im[i_1] = tmp_im1*ctheta - tmp_im0*stheta;
         }
         i_0+=gridDim.x*blockDim.x;
     }
@@ -599,10 +599,10 @@ void kernel_suqa_pauli_TP_rotation_z(double *const state_re, double *const state
             tmp_im1 = state_im[i_1];
             
             state_re[i_0] = tmp_re0*ctheta - tmp_im0*stheta;
-            state_im[i_0] = tmp_im0*stheta + tmp_re0*stheta;
+            state_im[i_0] = tmp_im0*ctheta + tmp_re0*stheta;
 
             state_re[i_1] = tmp_re1*ctheta + tmp_im1*stheta; 
-            state_im[i_1] = tmp_im1*stheta - tmp_re1*stheta;
+            state_im[i_1] = tmp_im1*ctheta - tmp_re1*stheta;
         }
         i_0+=gridDim.x*blockDim.x;
     }
