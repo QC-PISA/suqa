70219faefdbe4058da157ea8c1397a8fd229996c
joek93 <giuseppe.clemente93@gmail.com>
2020-04-17 10:49:33 +0200

    Added missing changes in src/test_evolution.cu

--- src/test_evolution.cu
+++ src/test_evolution.cu
@@ -8,14 +8,16 @@
 //#include <unistd.h>
 #include <cmath>
 #include <cassert>
+//#include "Rand.hpp"
 #include <chrono>
 #include <cuda.h>
 #include <cuda_runtime_api.h>
 #include <cuda_device_runtime_api.h>
+#include <device_launch_parameters.h>
 #include "io.hpp"
 #include "suqa.cuh"
 #include "system.cuh"
-#include "Rand.hpp"
+//#include "Rand.hpp"
 
 
 using namespace std;
@@ -26,8 +28,6 @@ uint suqa::threads;
 uint suqa::blocks;
 cudaStream_t suqa::stream1, suqa::stream2;
 
-const int Dim=6;
-
 void deallocate_state(ComplexVec& state){
     if(state.data!=nullptr){
         HANDLE_CUDACALL(cudaFree(state.data));
@@ -36,7 +36,7 @@ void deallocate_state(ComplexVec& state){
 }
 
 void allocate_state(ComplexVec& state, uint Dim){
-    if(state.data!=nullptr or Dim!=state.vecsize)
+    if(state.data!=nullptr || Dim!=state.vecsize)
         deallocate_state(state);
 
 
@@ -52,27 +52,27 @@ void self_plaquette(ComplexVec& state, const bmReg& qr0, const bmReg& qr1, const
 int main(int argc, char** argv){
     if(argc<5){
        printf("usage: %s <g_beta> <total_steps> <trotter_stepsize> <outfile>\n",argv[0]); 
-       exit(1);
+       exit(0);
     }
-   // g_beta = stod(argv[1]); // (extern) def in src/system.cu
+    g_beta = stod(argv[1]); // (extern) def in src/system.cu
     int total_steps = atoi(argv[2]);
     double trotter_stepsize = stod(argv[3]);
     string outfilename = argv[4];
 
-    //printf("arguments:\n g_beta = %.16lg\n total_steps = %d\n trotter_stepsize = %.16lg\n outfile = %s\n", g_beta, total_steps, trotter_stepsize, outfilename.c_str());
+    printf("arguments:\n g_beta = %.16lg\n total_steps = %d\n trotter_stepsize = %.16lg\n outfile = %s\n", g_beta, total_steps, trotter_stepsize, outfilename.c_str());
 
+    uint Dim = 1U << 13;
     suqa::threads = NUM_THREADS;
     suqa::blocks = (Dim+suqa::threads-1)/suqa::threads;
     if(suqa::blocks>MAXBLOCKS) suqa::blocks=MAXBLOCKS;
     printf("blocks: %u, threads: %u\n",suqa::blocks, suqa::threads);
 
     ComplexVec state;
-  
+
     allocate_state(state, Dim);
 
-    pcg rangen;
-    rangen.set_seed(time(NULL));
-    rangen.randint(0,3);
+//    pcg rangen;
+//    rangen.set_seed(time(NULL));
 
     suqa::setup(Dim);
     init_state(state, Dim);
@@ -85,7 +85,8 @@ int main(int argc, char** argv){
     for(uint ii=0; ii<=(uint)total_steps; ++ii){
         double t = ii*trotter_stepsize;
         printf("time %.16lg\n", t);
-
+        double plaq_val=0.0;
+        double plaq_val_std=0.0;
 //        for(uint hit=0; hit<(uint)num_hits; ++hit){
 //            printf("\thit %u\n", hit);
 //            init_state(state, Dim, g_beta);
@@ -105,25 +106,18 @@ int main(int argc, char** argv){
 //        fprintf(outfile, "%.16lg %d %.16lg %.16lg\n", t, num_hits, plaq_val, plaq_val_std);
 
         init_state(state, Dim);
-	suqa::apply_h(state,  bm_spin[rangen.randint(0,3)]);
         evolution(state, t, ii);
-        printf("random number= %d\n", rangen.randint(0,3));
+        self_plaquette(state, bm_qlink1, bm_qlink0, bm_qlink2, bm_qlink0);
 
-	//suqa::apply_h(state,  bm_spin[rangen.randint(0,3)]);
-	
-        double p000=0, p100=0, p010=0, p110=0, p001=0, p101=0, p011=0, p111=0;
-        suqa::prob_filter(state, bm_spin, {0U,0U,0U}, p000);
-        suqa::prob_filter(state, bm_spin, {1U,0U,0U}, p100);
-        suqa::prob_filter(state, bm_spin, {0U,1U,0U}, p010);
-        suqa::prob_filter(state, bm_spin, {1U,1U,0U}, p110);
-        suqa::prob_filter(state, bm_spin, {0U,0U,1U}, p001);
-        suqa::prob_filter(state, bm_spin, {1U,0U,1U}, p101);
-        suqa::prob_filter(state, bm_spin, {0U,1U,1U}, p011);
-        suqa::prob_filter(state, bm_spin, {1U,1U,1U}, p111);
-        printf("p000 = %.12lg; p100 = %.12lg\n", p000, p100);
+        double p000, p010;
+        suqa::prob_filter(state, bm_qlink1, {0U,0U,0U}, p000);
+        suqa::prob_filter(state, bm_qlink1, {0U,1U,0U}, p010);
+        printf("p000 = %.12lg; p010 = %.12lg\n", p000, p010);
+        plaq_val = 2.0*(p000-p010);
+        plaq_val_std = sqrt(4.0*(p000+p010)-plaq_val*plaq_val);
         outfile = fopen(outfilename.c_str(), "a");
-        fprintf(outfile, "%.12lg %.12lg %.12lg %.12lg %.12lg %.12lg %.12lg %.12lg %.12lg\n", t, p000,p100, p010,p110, p001,p101, p011,p111);
-
+        fprintf(outfile, "%.12lg %.12lg %.12lg\n", t, plaq_val, plaq_val_std);
+        printf("%.12lg %.12lg %.12lg\n", t, plaq_val, plaq_val_std);
         fclose(outfile);
     }
     
